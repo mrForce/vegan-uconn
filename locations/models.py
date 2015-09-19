cbeb0a89eb259bc2ef9bdd0ce63b918c84c76c19
hecka_tao <tao@btao.org>
2015-09-19 17:33:36 -0400

    fix Location field specification

--- locations/models.py
+++ locations/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=100)
     is_dining_hall = models.BooleanField(blank=False, default=False)
-    latitude = models.DecimalField(decimal_places=4)    # 4 decimal places = ~10m accuracy at the equator (worst case)
-    longitude = models.DecimalField(decimal_places=4)
+    # 4 decimal places = ~10m accuracy at the equator (worst case)
+    latitude = models.DecimalField(max_digits=7, decimal_places=4, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=7, decimal_places=4, null=True, blank=True)
+    url = models.URLField(max_length=300)
 
 # class OpeningHours(models.Model):
 #     WEEKDAYS = [
