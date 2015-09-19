7714748f29e3b0e7e556980ee92b5ba7e971c61f
hecka_tao <tao@btao.org>
2015-09-19 17:06:17 -0400

    create potential future opening hours model

--- locations/models.py
+++ locations/models.py
@@ -5,3 +5,27 @@ class Location(models.Model):
     is_dining_hall = models.BooleanField(blank=False, default=False)
     latitude = models.DecimalField(decimal_places=4)    # 4 decimal places = ~10m accuracy at the equator (worst case)
     longitude = models.DecimalField(decimal_places=4)
+
+# class OpeningHours(models.Model):
+#     WEEKDAYS = [
+#         (1, _("Monday")),
+#         (2, _("Tuesday")),
+#         (3, _("Wednesday")),
+#         (4, _("Thursday")),
+#         (5, _("Friday")),
+#         (6, _("Saturday")),
+#         (7, _("Sunday")),
+#     ]
+#     location = models.ForeignKey(Location)
+#     date = models.DateField(blank=True)                 # only fill this for special days, e.g. holidays
+#     weekday = models.IntegerField(choices=WEEKDAYS)     # used on normal days
+#     breakfast_from  = models.TimeField()
+#     breakfast_to    = models.TimeField()
+#     brunch_from     = models.TimeField()
+#     brunch_to       = models.TimeField()
+#     lunch_from      = models.TimeField()
+#     lunch_to        = models.TimeField()
+#     dinner_from     = models.TimeField()
+#     dinner_to       = models.TimeField()
+#     late_night_from = models.TimeField()
+#     late_night_to   = models.TimeField()
