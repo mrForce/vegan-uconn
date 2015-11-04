2c0a97b0e2ed5d90714674fb274d12d24310c55f
hecka_tao <tao@btao.org>
2015-11-03 20:28:37 -0500

    create basic index page

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from food import views as food_views
+
 """vegan_uconn URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +20,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', food_views.index, name="index")
 ]
---
+++ food/views.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+from django.shortcuts import render_to_response
+
+from locations.models import Location
+from food.models import Food
+from core.utils import current_meal_type, next_meal_type
+
+def index(request):
+    # TODO: add ability to show non-dining-hall places (cafes etc)
+    # TODO: add ability to sort by distance
+    # create a list of lists of lists in the format
+    # [
+    #   ["Place name 1",
+    #      i ["Entrees", entree_1, ..., entree_n],
+    #       ["Sides", side_1, ..., side_n]
+    #   ],
+    #   ["Place name 2",
+    #       ["Entrees", entree_1, ..., entree_n],
+    #       ["Desserts", dessert_1, ..., dessert_n]
+    #   ],
+    #   etc.
+    # ]
+    current_meal = current_meal_type()
+    next_meal = next_meal_type()
+
+    foods = Food.objects.filter(date=datetime.today()).filter(meal=current_meal).filter(is_vegan=True)
+    list_of_foods = [foods.filter(location=l) for l in Location.objects.filter(is_dining_hall=True)]
+
+    locations_dict = {}
+    for l in Location.objects.filter(is_dining_hall=True):
+        foods = Food.objects.filter(date=datetime.today()).filter(meal=current_meal)\
+                                                          .filter(is_vegan=True)\
+                                                          .filter(location=l)
+        if foods.count() == 0:
+            break
+        categories = foods.values_list(flat=True).distinct()
+        categories_dict = {}
+
+        for category in categories:
+            foods_list = foods.filter(category=category)
+            categories_dict[category] = foods_list
+            locations_dict[l.name] = categories_dict
+
+    return render_to_response("index.html", {"locations_dict": locations_dict,
+                                             "current_meal": current_meal,
+                                             "next_meal": next_meal,
+                                             "now": datetime.now(),
+                                             "categories": categories})
