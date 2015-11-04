fc49ff0c61880513fa83c78f9c464c904e49ba60
hecka_tao <tao@btao.org>
2015-11-03 20:30:10 -0500

    add open_now function to Location

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 class Location(models.Model):
@@ -7,6 +9,18 @@ class Location(models.Model):
     latitude = models.DecimalField(max_digits=7, decimal_places=4, null=True, blank=True)
     longitude = models.DecimalField(max_digits=7, decimal_places=4, null=True, blank=True)
     url = models.URLField(max_length=300)
+
+    def open_now(self):
+        now = datetime.now()
+        hours = OpeningHours.objects.filter(location=self, weekday=now.isoweekday())
+        if (hours.breakfast_from < now.time() < hours.breakfast_to) or \
+           (hours.lunch_from < now.time() < hours.lunch_to) or \
+           (hours.dinner_from < now.time() < hours.dinner_to) or \
+           (hours.late_night_from < now.time() < hours.late_night_to):
+            return True
+        else:
+            return False
+
 
 class OpeningHours(models.Model):
     WEEKDAYS = [
